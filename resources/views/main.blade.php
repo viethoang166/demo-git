86a776ce73249ebcb688b65f880aef2b78c26741
NuS2k <ngoduong543@gmail.com>
2022-07-18 17:09:53 +0700

    day1

---
+++ resources/views/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    @include ('layout.head')
+</head>
+<body>
+    @include ('layout.header')
+    <div class="container">
+        @include ('layout.sidebar')
+        @yield('content')
+    </div>
+
+    @include ('layout.footer')
+</body>
+</html>
